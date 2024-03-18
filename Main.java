ead13dc3c54790aed51609b31dd25640a587abb0
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-03-18 08:47:30 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.mycompany.televisao;
+
+class Main {
+
+    public static void main(String[] args) {
+        
+        Televisao tv01 = new Televisao();
+        tv01.ligar();
+        tv01.desligar();
+        
+        Televisao tv02 = new Televisao();
+        tv02.ligar();
+    }
+
+}
